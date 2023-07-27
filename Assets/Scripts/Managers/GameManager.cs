4cb04f66025b92da049e952d572a50911e84a27c
Emre Hün <the.emrehun@gmail.com>
2023-07-27 13:08:22 +0300

    Added GameManager Functions

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,23 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace BTK_Academy_Tetris_Managers
 {
     public class GameManager : MonoBehaviour
     {
         SpawnManager _spawnManager;
         BoardManager _boardManager;
+
+        ShapeManager _activeShape;
         void Start()
         {
             _spawnManager= GameObject.FindObjectOfType<SpawnManager>();
             _boardManager= GameObject.FindObjectOfType<BoardManager>();
+
+            if (_spawnManager)
+            {
+                if(_activeShape == null)
+                {
+                    _activeShape = _spawnManager.SpawnShapes();
+                    _activeShape.transform.position = VectorToPrecisionNumber(_activeShape.transform.position);
+                }
+            }
         }
 
-        
         void Update()
         {
+            if(!_spawnManager || !_boardManager)
+            {
+                return;
+            }
 
+            if (_activeShape)
+            {
+                _activeShape.MoveDown();
+            }
+               
         }
+        Vector2 VectorToPrecisionNumber(Vector2 vec) {
+
+            return new Vector2(Mathf.Round(vec.x), Mathf.Round(vec.y));
+        }
+
     }
 
 }
